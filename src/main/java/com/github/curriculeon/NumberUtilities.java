6797a337ca3f559c159484cf45c8e4a8de80726a
git-leon <xleonhunter@gmail.com>
2021-06-26 10:30:59 -0400

    formatted text

--- src/main/java/com/github/curriculeon/NumberUtilities.java
+++ src/main/java/com/github/curriculeon/NumberUtilities.java
@@ -8,13 +8,11 @@ public class NumberUtilities {
                 getRange(start, stop, 2);
     }
 
-
     public static String getOddNumbers(int start, int stop) {
         return start % 2 == 0 ?
                 getRange(++start, stop, 2) :
                 getRange(start, stop, 2);
     }
-
 
     public static String getSquareNumbers(int start, int stop, int step) {
         return getExponentiations(start, stop, step, 2);
@@ -28,7 +26,6 @@ public class NumberUtilities {
         return getRange(start, stop, 1);
 
     }
-
 
     public static String getRange(int start, int stop, int step) {
         return getExponentiations(start, stop, step, 1);
