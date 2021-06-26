f6ee6795fd7996b6595948766645ac87540297e6
git-leon <xleonhunter@gmail.com>
2021-06-26 10:34:25 -0400

    merged with master's NumberUtilities

--- src/main/java/com/github/curriculeon/NumberUtilities.java
+++ src/main/java/com/github/curriculeon/NumberUtilities.java
@@ -3,42 +3,34 @@ package com.github.curriculeon;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return start % 2 == 1 ?
-                getRange(++start, stop, 2) :
-                getRange(start, stop, 2);
+        return null;
     }
+
 
     public static String getOddNumbers(int start, int stop) {
-        return start % 2 == 0 ?
-                getRange(++start, stop, 2) :
-                getRange(start, stop, 2);
+        return null;
     }
+
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return getExponentiations(start, stop, step, 2);
+        return null;
     }
 
-    public static String getRange(int stop) {
-        return getRange(0, stop);
+    public static String getRange(int start) {
+        return null;
     }
 
     public static String getRange(int start, int stop) {
-        return getRange(start, stop, 1);
-
+        return null;
     }
 
+
     public static String getRange(int start, int stop, int step) {
-        return getExponentiations(start, stop, step, 1);
+        return null;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        int currentNumber = start;
-        StringBuilder range = new StringBuilder();
-        while (currentNumber < stop) {
-            range.append((int) Math.pow(currentNumber, exponent));
-            currentNumber += step;
-        }
-        return range.toString();
+        return null;
     }
 }
