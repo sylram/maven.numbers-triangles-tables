b5578b929f7330a5e7bdb0c3565264d9c004eaac
git-leon <xleonhunter@gmail.com>
2021-06-26 10:36:11 -0400

    merged with master's triangleutilities

--- src/main/java/com/github/curriculeon/TriangleUtilities.java
+++ src/main/java/com/github/curriculeon/TriangleUtilities.java
@@ -1,27 +1,20 @@
 package com.github.curriculeon;
 
-
 public class TriangleUtilities {
 
-    public static String getRow(int numberOfStars) {
-        return new String(new char[numberOfStars]).replaceAll("\u0000", "*");
+    public static String getTriangle(int numberOfRows) {
+        return null;
     }
 
-    public static String getTriangle(int numberOfRows) {
-        String triangle="";
-        int currentRow=1;
-        while (currentRow<numberOfRows  ){
-            triangle+=getRow(currentRow)+"\n";
-            currentRow++;
-        }
-        return triangle;
+    public static String getRow(int numberOfStars) {
+        return null;
     }
 
     public static String getSmallTriangle() {
-        return getTriangle(5);
+        return null;
     }
 
     public static String getLargeTriangle() {
-        return getTriangle(10);
+        return null;
     }
 }
